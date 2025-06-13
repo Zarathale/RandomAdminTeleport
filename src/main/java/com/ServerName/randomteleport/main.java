07fab6962469494fbf50ec7f54d8d85057c8f0c4
Zarathale <alytle@mac.com>
2025-06-12 17:00:34 -0700

    Uploading initial build for first review

---
+++ src/main/java/com/ServerName/randomteleport/main.java
@@ -0,0 +1,17 @@
+package com.luca.randomteleport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("rtpadmin").setExecutor(new RandomTeleportCommand());
+        getLogger().info("RandomTeleport enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("RandomTeleport disabled.");
+    }
+}
