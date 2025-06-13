1d6234ee6ed16d55463b324506307f2df4e35c61
Zarathale <alytle@mac.com>
2025-06-12 19:24:48 -0700

    changed to shared instance for random

---
+++ src/main/java/com/ServerName/randomteleport/RandomTeleportCommand.java
@@ -0,0 +1,44 @@
+package com.luca.randomteleport;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+private static final Random RANDOM = new Random();
+
+public class RandomTeleportCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        Player admin = (Player) sender;
+
+        if (!admin.hasPermission("randomteleport.use")) {
+            admin.sendMessage("You do not have permission to use this command.");
+            return true;
+        }
+
+        List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+        players.remove(admin);
+
+        if (players.isEmpty()) {
+            admin.sendMessage("No other players are online to teleport to.");
+            return true;
+        }
+
+        Player target = players.get(RANDOM.nextInt(players.size()));
+        admin.teleport(target.getLocation());
+        admin.sendMessage("You have been teleported to: " + target.getName());
+        return true;
+    }
+}
